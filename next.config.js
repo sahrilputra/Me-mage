170acded82b284418fe646b3fd8a9288eded9476
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2023-03-29 23:14:55 +0700

    second commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [{
+        protocol: "https",
+        hostname: "replicate.com",
+      },
+      {
+        protocol: "https",
+        hostname: "replicate.delivery",
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
